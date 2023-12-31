83594e8ab8454dace6d6b9db135684d0f9f2692d
suurkivi <136639517+suurkivi@users.noreply.github.com>
2023-12-31 14:12:38 -0800

    change order of probes

--- cmd/reflowoven/main.go
+++ cmd/reflowoven/main.go
@@ -58,8 +58,8 @@ func main() {
 	}
 
 	tcs := []*Thermocouple{
-		NewThermocouple(log, bus, 0x60, "probe0"),
-		NewThermocouple(log, bus, 0x67, "probe1"),
+		NewThermocouple(log, bus, 0x67, "probe0"),
+		NewThermocouple(log, bus, 0x60, "probe1"),
 	}
 
 	cook := gpioreg.ByName("GPIO16")
